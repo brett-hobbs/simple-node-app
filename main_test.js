801738107e101ff8ab10f3002152fa684a7e124e
Brett Hobbs <brett.hobbs@gmail.com>
2017-02-09 21:13:47 -0800

    Add deps to package.json

--- main_test.js
+++ main_test.js
@@ -1,9 +1,5 @@
 'use strict';
-const chai = require('chai');
-const sinon = require('sinon');
 const main = require('./main.js');
-
-const assert = chai.assert;
 
 describe('Test stub', () => {
   it('returns hello word', () => {
---
+++ test/setup.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const promise = require('bluebird');
+const sinon = require('sinon');
+
+global.sinon = sinon;
+global.assert = chai.assert;
